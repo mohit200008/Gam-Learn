e59ea4c4e0c5ffd761713ee9e33aed6fad524a20
shmiitg <shubhamank002@gmail.com>
2021-12-09 23:37:55 +0530

    get questions from database

--- frontend/LandingPage/Questions/History/cards.js
+++ frontend/LandingPage/Questions/History/cards.js
@@ -1,206 +1,255 @@
-var scenarios = [
-  {
-    questCard: {
-      description: "All India Harijan Sangha established in? ",
-      power: 4,
-    },
-    playerCards: [
-      {
-        description: "1932",
-        power: 5,
-      },
-      {
-        description: "1925",
-        power: 3,
-      },
-      {
-        description: "1825",
-        power: 1,
-      }
-    ]
-  },
-  {
-    questCard: {
-      description: "Mangal Pandey was a sepoy at?",
-      power: 3,
-    },
-    playerCards: [
-      {
-        description: "Sikh Regiment",
-        power: 5,
-      },
-      {
-        description: "34th Bengal Native Infantary",
-        power: 4,
-      },
-      {
-        description: "Royal Gorkha Rifle",
-        power: 1,
-      }
-    ]
-  },
-  {
-    questCard: {
-      description: "First battle of Panipat was fought between?",
-      power: 3,
-    },
-    playerCards: [
-      {
-        description: "Akbar and Hemu",
-        power: 5,
-      },
-      {
-        description: "Babur and Lodi",
-        power: 4,
-      },
-      {
-        description: "Akbar and Lodi",
-        power: 1,
-      }
-    ]
-  },
-  {
-    questCard: {
-      description: "Second battle of Panipat was fought between? ",
-      power: 3,
-    },
-    playerCards: [
-      {
-        description: "Akbar and Hemu",
-        power: 4,
-      },
-      {
-        description: "Mughal and British",
-        power: 2,
-      },
-      {
-        description: "Akbar and Lodi",
-        power: 1,
-      }
-    ]
-  },
-  {
-    questCard: {
-      description: " Who was the founder of Madras?",
-      power: 2,
-    },
-    playerCards: [
-      {
-        description: "Lord Dalhousie",
-        power: 4,
-      },
-      {
-        description: "Francis Day",
-        power: 3,
-      },
-      {
-        description: "Robert Clive",
-        power: 1,
-      }
-    ]
-  },
+const firebaseConfig = {
+  apiKey: "AIzaSyCI6w4PPKKecXzYh6QHG6yaXKdSDALSqPE",
+  authDomain: "gam-learn-9af98.firebaseapp.com",
+  projectId: "gam-learn-9af98",
+  storageBucket: "gam-learn-9af98.appspot.com",
+  messagingSenderId: "517281640775",
+};
 
-  {
-    questCard: {
-      description: "Who is writer of Geet Govind ",
-      power: 2,
-    },
-    playerCards: [
-      {
-        description: "Ravidas",
-        power: 4,
-      },
-      {
-        description: "Jayadev",
-        power: 3,
-      },
-      {
-        description: "Kalidas",
-        power: 1,
-      }
-    ]
-  },
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
 
-  {
-    questCard: {
-      description: "Battle of Buxar was fought in?",
-      power: 2,
-    },
-    playerCards: [
-      {
-        description: "1958",
-        power: 4,
-      },
-      {
-        description: "1964",
-        power: 3,
-      },
-      {
-        description: "1900",
-        power: 1,
-      }
-    ]
-  },
-  {
-    questCard: {
-      description: "Swami Vivekananda attended the Parliament of the World's Religions in America in the year?",
-      power: 3,
-    },
-    playerCards: [
-      {
-        description: "1893",
-        power: 4,
-      },
-      {
-        description: "1815",
-        power: 2,
-      },
-      {
-        description: "1855",
-        power: 1,
-      }
+db.settings({ timestampsInSnapshots: true });
 
-    ]
-  },
-  {
-    questCard: {
-      description: "Chandragupta was succeeded by? ",
-      power: 2,
-    },
-    playerCards: [
-      {
-        description: "Hemusara",
-        power: 4,
-      },
-      {
-        description: "Bindusara",
-        power: 3,
-      },
-      {
-        description: "Ashoka",
-        power: 1,
-      },
-    ]
-  },
-  {
-    questCard: {
-      description: "What is meaning of Mati Jnana in Jainism?",
-      power: 2,
-    },
-    playerCards: [
-      {
-        description: "Clairvoyant perception",
-        power: 4,
-      },
-      {
-        description: "Perception through activity of sense organs.",
-        power: 3,
-      },
-      {
-        description: "Telepathic knowledge",
-        power: 1,
-      }
+var scenarios = [];
 
-    ]
-  }
-];
+const getQuestions = async () => {
+  const snapshot = await db.collection("history");
+  snapshot.get().then(query => {
+    const tempDoc = query.docs.map(doc => doc.data())
+    tempDoc.forEach(temp => {
+      const obj1 = {
+        questCard: {
+          description: temp.questDescription,
+          power: temp.questPower
+        }
+      }
+      const obj2 = {
+        playerCards: [
+          {
+            description: temp.playerDescription[0],
+            power: temp.playerPower[0]
+          }, {
+            description: temp.playerDescription[1],
+            power: temp.playerPower[1]
+          }, {
+            description: temp.playerDescription[2],
+            power: temp.playerPower[2]
+          }
+        ]
+      }
+      let obj = { ...obj1, ...obj2 };
+      scenarios.push(obj);
+    })
+  })
+}
+
+getQuestions();
+
+// var scenarios = [
+//   {
+//     questCard: {
+//       description: "All India Harijan Sangha established in? ",
+//       power: 4,
+//     },
+//     playerCards: [
+//       {
+//         description: "1932",
+//         power: 5,
+//       },
+//       {
+//         description: "1925",
+//         power: 3,
+//       },
+//       {
+//         description: "1825",
+//         power: 1,
+//       }
+//     ]
+//   },
+//   {
+//     questCard: {
+//       description: "Mangal Pandey was a sepoy at?",
+//       power: 3,
+//     },
+//     playerCards: [
+//       {
+//         description: "Sikh Regiment",
+//         power: 5,
+//       },
+//       {
+//         description: "34th Bengal Native Infantary",
+//         power: 4,
+//       },
+//       {
+//         description: "Royal Gorkha Rifle",
+//         power: 1,
+//       }
+//     ]
+//   },
+//   {
+//     questCard: {
+//       description: "First battle of Panipat was fought between?",
+//       power: 3,
+//     },
+//     playerCards: [
+//       {
+//         description: "Akbar and Hemu",
+//         power: 5,
+//       },
+//       {
+//         description: "Babur and Lodi",
+//         power: 4,
+//       },
+//       {
+//         description: "Akbar and Lodi",
+//         power: 1,
+//       }
+//     ]
+//   },
+//   {
+//     questCard: {
+//       description: "Second battle of Panipat was fought between? ",
+//       power: 3,
+//     },
+//     playerCards: [
+//       {
+//         description: "Akbar and Hemu",
+//         power: 4,
+//       },
+//       {
+//         description: "Mughal and British",
+//         power: 2,
+//       },
+//       {
+//         description: "Akbar and Lodi",
+//         power: 1,
+//       }
+//     ]
+//   },
+//   {
+//     questCard: {
+//       description: " Who was the founder of Madras?",
+//       power: 2,
+//     },
+//     playerCards: [
+//       {
+//         description: "Lord Dalhousie",
+//         power: 4,
+//       },
+//       {
+//         description: "Francis Day",
+//         power: 3,
+//       },
+//       {
+//         description: "Robert Clive",
+//         power: 1,
+//       }
+//     ]
+//   },
+
+//   {
+//     questCard: {
+//       description: "Who is writer of Geet Govind ",
+//       power: 2,
+//     },
+//     playerCards: [
+//       {
+//         description: "Ravidas",
+//         power: 4,
+//       },
+//       {
+//         description: "Jayadev",
+//         power: 3,
+//       },
+//       {
+//         description: "Kalidas",
+//         power: 1,
+//       }
+//     ]
+//   },
+
+//   {
+//     questCard: {
+//       description: "Battle of Buxar was fought in?",
+//       power: 2,
+//     },
+//     playerCards: [
+//       {
+//         description: "1958",
+//         power: 4,
+//       },
+//       {
+//         description: "1964",
+//         power: 3,
+//       },
+//       {
+//         description: "1900",
+//         power: 1,
+//       }
+//     ]
+//   },
+//   {
+//     questCard: {
+//       description: "Swami Vivekananda attended the Parliament of the World's Religions in America in the year?",
+//       power: 3,
+//     },
+//     playerCards: [
+//       {
+//         description: "1893",
+//         power: 4,
+//       },
+//       {
+//         description: "1815",
+//         power: 2,
+//       },
+//       {
+//         description: "1855",
+//         power: 1,
+//       }
+
+//     ]
+//   },
+//   {
+//     questCard: {
+//       description: "Chandragupta was succeeded by? ",
+//       power: 2,
+//     },
+//     playerCards: [
+//       {
+//         description: "Hemusara",
+//         power: 4,
+//       },
+//       {
+//         description: "Bindusara",
+//         power: 3,
+//       },
+//       {
+//         description: "Ashoka",
+//         power: 1,
+//       },
+//     ]
+//   },
+//   {
+//     questCard: {
+//       description: "What is meaning of Mati Jnana in Jainism?",
+//       power: 2,
+//     },
+//     playerCards: [
+//       {
+//         description: "Clairvoyant perception",
+//         power: 4,
+//       },
+//       {
+//         description: "Perception through activity of sense organs.",
+//         power: 3,
+//       },
+//       {
+//         description: "Telepathic knowledge",
+//         power: 1,
+//       }
+
+//     ]
+//   }
+// ];
--- frontend/LandingPage/Questions/Maths/cards.js
+++ frontend/LandingPage/Questions/Maths/cards.js
@@ -1,206 +1,255 @@
-var scenarios = [
-  {
-    questCard: {
-      description: "What is the average of first 150 natural numbers?",
-      power: 4,
-    },
-    playerCards: [
-      {
-        description: "75.5",
-        power: 5,
-      },
-      {
-        description: "25",
-        power: 3,
-      },
-      {
-        description: "25",
-        power: 1,
-      }
-    ]
-  },
-  {
-    questCard: {
-      description: "What is the sum of 130+125+191",
-      power: 3,
-    },
-    playerCards: [
-      {
-        description: "329",
-        power: 5,
-      },
-      {
-        description: "446",
-        power: 4,
-      },
-      {
-        description: "105",
-        power: 1,
-      }
-    ]
-  },
-  {
-    questCard: {
-      description: "If we minus 712 from 1500, how much do we get?",
-      power: 3,
-    },
-    playerCards: [
-      {
-        description: "488",
-        power: 5,
-      },
-      {
-        description: "788",
-        power: 4,
-      },
-      {
-        description: "399",
-        power: 1,
-      }
-    ]
-  },
-  {
-    questCard: {
-      description: "50 times of 8 is equal to: ",
-      power: 3,
-    },
-    playerCards: [
-      {
-        description: "400",
-        power: 4,
-      },
-      {
-        description: "288",
-        power: 2,
-      },
-      {
-        description: "3666",
-        power: 1,
-      }
-    ]
-  },
-  {
-    questCard: {
-      description: " 110 divided by 10 is:",
-      power: 2,
-    },
-    playerCards: [
-      {
-        description: "44",
-        power: 4,
-      },
-      {
-        description: "11",
-        power: 3,
-      },
-      {
-        description: "99",
-        power: 1,
-      }
-    ]
-  },
+const firebaseConfig = {
+  apiKey: "AIzaSyCI6w4PPKKecXzYh6QHG6yaXKdSDALSqPE",
+  authDomain: "gam-learn-9af98.firebaseapp.com",
+  projectId: "gam-learn-9af98",
+  storageBucket: "gam-learn-9af98.appspot.com",
+  messagingSenderId: "517281640775",
+};
 
-  {
-    questCard: {
-      description: "20+(90÷2) is equal to:",
-      power: 2,
-    },
-    playerCards: [
-      {
-        description: "44",
-        power: 4,
-      },
-      {
-        description: "65",
-        power: 3,
-      },
-      {
-        description: "45",
-        power: 1,
-      }
-    ]
-  },
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
 
-  {
-    questCard: {
-      description: "The product of 82 and 5 is: ",
-      power: 2,
-    },
-    playerCards: [
-      {
-        description: "258",
-        power: 4,
-      },
-      {
-        description: "410",
-        power: 3,
-      },
-      {
-        description: "300",
-        power: 1,
-      }
-    ]
-  },
-  {
-    questCard: {
-      description: "Find the missing terms in multiple of 3: 3, 6, 9, __, 15 ",
-      power: 3,
-    },
-    playerCards: [
-      {
-        description: "12",
-        power: 4,
-      },
-      {
-        description: "15",
-        power: 2,
-      },
-      {
-        description: "55",
-        power: 1,
-      }
+db.settings({ timestampsInSnapshots: true });
 
-    ]
-  },
-  {
-    questCard: {
-      description: "Solve 24÷8+2.",
-      power: 2,
-    },
-    playerCards: [
-      {
-        description: "8",
-        power: 4,
-      },
-      {
-        description: "5",
-        power: 3,
-      },
-      {
-        description: "12",
-        power: 1,
-      },
-    ]
-  },
-  {
-    questCard: {
-      description: "The product of 121 x 0 x 200 x 25 is",
-      power: 2,
-    },
-    playerCards: [
-      {
-        description: "4",
-        power: 4,
-      },
-      {
-        description: "0",
-        power: 3,
-      },
-      {
-        description: "1",
-        power: 1,
-      }
+var scenarios = [];
 
-    ]
-  }
-];
+const getQuestions = async () => {
+  const snapshot = await db.collection("maths");
+  snapshot.get().then(query => {
+    const tempDoc = query.docs.map(doc => doc.data())
+    tempDoc.forEach(temp => {
+      const obj1 = {
+        questCard: {
+          description: temp.questDescription,
+          power: temp.questPower
+        }
+      }
+      const obj2 = {
+        playerCards: [
+          {
+            description: temp.playerDescription[0],
+            power: temp.playerPower[0]
+          }, {
+            description: temp.playerDescription[1],
+            power: temp.playerPower[1]
+          }, {
+            description: temp.playerDescription[2],
+            power: temp.playerPower[2]
+          }
+        ]
+      }
+      let obj = { ...obj1, ...obj2 };
+      scenarios.push(obj);
+    })
+  })
+}
+
+getQuestions();
+
+// var scenarios = [
+//   {
+//     questCard: {
+//       description: "What is the average of first 150 natural numbers?",
+//       power: 4,
+//     },
+//     playerCards: [
+//       {
+//         description: "75.5",
+//         power: 5,
+//       },
+//       {
+//         description: "25",
+//         power: 3,
+//       },
+//       {
+//         description: "25",
+//         power: 1,
+//       }
+//     ]
+//   },
+//   {
+//     questCard: {
+//       description: "What is the sum of 130+125+191",
+//       power: 3,
+//     },
+//     playerCards: [
+//       {
+//         description: "329",
+//         power: 5,
+//       },
+//       {
+//         description: "446",
+//         power: 4,
+//       },
+//       {
+//         description: "105",
+//         power: 1,
+//       }
+//     ]
+//   },
+//   {
+//     questCard: {
+//       description: "If we minus 712 from 1500, how much do we get?",
+//       power: 3,
+//     },
+//     playerCards: [
+//       {
+//         description: "488",
+//         power: 5,
+//       },
+//       {
+//         description: "788",
+//         power: 4,
+//       },
+//       {
+//         description: "399",
+//         power: 1,
+//       }
+//     ]
+//   },
+//   {
+//     questCard: {
+//       description: "50 times of 8 is equal to: ",
+//       power: 3,
+//     },
+//     playerCards: [
+//       {
+//         description: "400",
+//         power: 4,
+//       },
+//       {
+//         description: "288",
+//         power: 2,
+//       },
+//       {
+//         description: "3666",
+//         power: 1,
+//       }
+//     ]
+//   },
+//   {
+//     questCard: {
+//       description: " 110 divided by 10 is:",
+//       power: 2,
+//     },
+//     playerCards: [
+//       {
+//         description: "44",
+//         power: 4,
+//       },
+//       {
+//         description: "11",
+//         power: 3,
+//       },
+//       {
+//         description: "99",
+//         power: 1,
+//       }
+//     ]
+//   },
+
+//   {
+//     questCard: {
+//       description: "20+(90÷2) is equal to:",
+//       power: 2,
+//     },
+//     playerCards: [
+//       {
+//         description: "44",
+//         power: 4,
+//       },
+//       {
+//         description: "65",
+//         power: 3,
+//       },
+//       {
+//         description: "45",
+//         power: 1,
+//       }
+//     ]
+//   },
+
+//   {
+//     questCard: {
+//       description: "The product of 82 and 5 is: ",
+//       power: 2,
+//     },
+//     playerCards: [
+//       {
+//         description: "258",
+//         power: 4,
+//       },
+//       {
+//         description: "410",
+//         power: 3,
+//       },
+//       {
+//         description: "300",
+//         power: 1,
+//       }
+//     ]
+//   },
+//   {
+//     questCard: {
+//       description: "Find the missing terms in multiple of 3: 3, 6, 9, __, 15 ",
+//       power: 3,
+//     },
+//     playerCards: [
+//       {
+//         description: "12",
+//         power: 4,
+//       },
+//       {
+//         description: "15",
+//         power: 2,
+//       },
+//       {
+//         description: "55",
+//         power: 1,
+//       }
+
+//     ]
+//   },
+//   {
+//     questCard: {
+//       description: "Solve 24÷8+2.",
+//       power: 2,
+//     },
+//     playerCards: [
+//       {
+//         description: "8",
+//         power: 4,
+//       },
+//       {
+//         description: "5",
+//         power: 3,
+//       },
+//       {
+//         description: "12",
+//         power: 1,
+//       },
+//     ]
+//   },
+//   {
+//     questCard: {
+//       description: "The product of 121 x 0 x 200 x 25 is",
+//       power: 2,
+//     },
+//     playerCards: [
+//       {
+//         description: "4",
+//         power: 4,
+//       },
+//       {
+//         description: "0",
+//         power: 3,
+//       },
+//       {
+//         description: "1",
+//         power: 1,
+//       }
+
+//     ]
+//   }
+// ];
